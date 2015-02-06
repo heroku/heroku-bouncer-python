4ac5379d6da137c0ce4c616da0773e8dbe155b90
Jacob Kaplan-Moss <jacob@jacobian.org>
2015-02-06 11:42:57 -0800

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "heroku-bouncer",
     description = "WSGI middleware that requires Heroku OAuth for all requests.",
-    version = "1.2",
+    version = "1.3",
     author = "Jacob Kaplan-Moss",
     author_email = "jacob@heroku.com",
     url = "http://github.com/heroku/heroku-bouncer-python",
